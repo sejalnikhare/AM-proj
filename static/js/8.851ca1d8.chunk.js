70abb45ebb3646b3d90925639bf85f7f21b76a21
sejal-angularminds <sejal@amdev.in>
2023-02-28 17:02:04 +0530

    Updates

---
+++ static/js/8.851ca1d8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpassignment-am"]=this["webpackJsonpassignment-am"]||[]).push([[8],{692:function(n,s,e){"use strict";e.r(s);e(0);var t=e(2);s.default=function(){return Object(t.jsx)(t.Fragment,{children:Object(t.jsx)("h1",{children:"Dashboared components"})})}}}]);
+//# sourceMappingURL=8.851ca1d8.chunk.js.map
